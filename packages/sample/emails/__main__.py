16c5cfb354757bbe235da3d7f0dfcc31fc908c89
alper-customhub <130510524+alper-customhub@users.noreply.github.com>
2023-05-17 14:37:37 -0700

    apikey

--- packages/sample/emails/__main__.py
+++ packages/sample/emails/__main__.py
@@ -1,60 +1,65 @@
-from http import HTTPStatus
+from PIL import Image
+import requests
 import os
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+Image.MAX_IMAGE_PIXELS = None
 
-def main(args):
-    '''
-    Takes in the email address, subject, and message to send an email using SendGrid, 
-    returns a json response letting the user know if the email sent or failed to send.
 
-        Parameters:
-            args: Contains the from email address, to email address, subject and message to send
+# Function to resize images
+def resize_image(image_url, width):
+    response = requests.get(image_url)
+    image = Image.open(response.content)
+    aspect_ratio = float(image.height) / float(image.width)
+    height = int(width * aspect_ratio)
+    return image.resize((width, height), Image.LANCZOS)
 
-        Returns:
-            json body: Json response if the email sent successfully or if an error happened
-    '''
-    key = os.getenv('API_KEY')
-    user_from = args.get("from")
-    user_to = args.get("to")
-    user_subject = args.get("subject")
-    content = args.get("content")
+# Load all image urls from a list
+image_urls = ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Tux.svg/300px-Tux.svg.png',
+               'https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/WikiProject_Zoo_Logo.svg/300px-WikiProject_Zoo_Logo.svg.png',
+               'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Tux.svg/300px-Tux.svg.png']
 
-    if not user_from:
-        return {
-            "statusCode" : HTTPStatus.BAD_REQUEST,
-            "body" : "no user email provided"
-        }
-    if not user_to:
-        return {
-            "statusCode" : HTTPStatus.BAD_REQUEST,
-            "body" : "no receiver email provided"
-        }
-    if not user_subject:
-        return {
-            "statusCode" : HTTPStatus.BAD_REQUEST,
-            "body" : "no subject provided"
-        }
-    if not content:
-        return {
-            "statusCode" : HTTPStatus.BAD_REQUEST,
-            "body" : "no content provided"
-        }
+# Resize images and store them in a list
+resized_images = [resize_image(image_url, int(10 * 300)) for image_url in image_urls]
 
-    sg = SendGridAPIClient(key)
-    message = Mail(
-        from_email = user_from,
-        to_emails = user_to,
-        subject = user_subject,
-        html_content = content)
-    response = sg.send(message)
+# Function to create a new canvas and reset offsets
+def create_canvas():
+    canvas = Image.new('RGBA', (canvas_width, canvas_height), (255, 255, 255, 0))
+    x_offset = 0
+    y_offset = 0
+    previous_resized_image_height = 0
+    return canvas, x_offset, y_offset, previous_resized_image_height
 
-    if response.status_code != 202:
-        return {
-            "statusCode" : response.status_code,
-            "body" : "email failed to send"
-        }
-    return {
-        "statusCode" : HTTPStatus.ACCEPTED,
-        "body" : "success"
-    }
+# Create a transparent canvas
+canvas_width = int(22 * 300)
+canvas_height = int(99 * 300)
+canvas, x_offset, y_offset, previous_resized_image_height = create_canvas()
+
+# Initialize variables
+spacing = int(0.5 * 300)
+previous_resized_image_height = 0
+canvas_count = 1
+
+def save_canvas(canvas, canvas_count):
+    canvas.save(f'output_{canvas_count}.png', format='PNG', dpi=(300, 300))
+
+# Arrange images on the canvas
+for resized_image in resized_images:
+    if y_offset + resized_image.height > canvas_height-1500:
+        save_canvas(canvas, canvas_count)
+        canvas_count += 1
+        canvas, x_offset, y_offset,previous_resized_image_height = create_canvas()
+
+    if x_offset + resized_image.width > canvas_width:
+        x_offset = 0
+        y_offset += previous_resized_image_height + spacing
+        previous_resized_image_height = 0
+
+    canvas.paste(resized_image, (x_offset, y_offset), resized_image)
+    x_offset += resized_image.width + spacing
+    if previous_resized_image_height < resized_image.height:
+        previous_resized_image_height = resized_image.height
+
+# Save the last canvas
+save_canvas(canvas, canvas_count)
+
+# Download the last canvas to the web browser
+webbrowser.open(f'file:///home/{os.getlogin()}/output_{canvas_count}.png')
